01bdd2df137961fa4ad442e77ec50ab38b80e1b6
Kqpa <76552592+Kqpa@users.noreply.github.com>
2022-04-12 18:50:09 +0300

    (actual) Initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+<div align="center">
+
+# Vector Utilities (Vutil)
+Vutil is a header file that makes it easier to interact and work with C++ Vectors. Currently work-in-progress — don't expect much.
+
+</div>
+
+# Features
+
+* **Printing 2D & 1D Vectors in a human-readable format**
+
+  * Usage:
+  
+  <br>
+  
+  ```cpp
+  /* A 2D int Vector */
+  std::vector<std::vector<int>> vector2D = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12} };
+
+  /* Printing the 2D Vector using vutil */
+  vutil::print_2d_vector<int>(vector2D);
+  ```
+  
+  * Outputs:
+  
+  <br>
+  
+  ```
+  (0) |   1   2   3   4
+  (1) |   5   6   7   8
+  (2) |   9  10  11  12
+  ```
+  
+* **Getting the size of a 2D Vector**
+
+  * Usage:
+  
+  <br>
+  
+  ```cpp
+  /* A 2D int Vector */
+  std::vector<std::vector<int>> vector2D = { {1, 2, 3, 4}, {5, 6}, {9, 10, 12} };
+
+  /* Getting the size using vutil */
+  int vector2D_size = vutil::calculate_2d_vector_size<int>(vector2D);
+
+  /* Printing the size */
+  std::cout << vector2D_size;
+  ```
+  
+  * Outputs:
+  
+  <br>
+  
+  ```
+  9
+  ```
+  
+# Installation
+**1**. `cd` into your project root
+
+**2**. make a `lib` folder
+
+**3**. `cd` into `lib`
+
+**4**. Run `curl https://raw.githubusercontent.com/Kqpa/vutil/blob/master/src/vutil.hpp`
+
+**5**. Include Vutil by putting `#include "./lib/vutil.hpp"` at the top of your source file
---
+++ src/vutil.hpp
@@ -0,0 +1,62 @@
+/**
+ * @file    vutil.hpp
+ * @author  Kqpa <kqpa@pm.me>
+ * @brief   Utility header to make it easier to work with Vectors
+**/
+
+#ifndef VUITL_HPP
+#define VUTIL_HPP
+
+#include <vector>
+#include <iostream>
+#include <iomanip>
+
+namespace vutil {
+
+    template <typename T>
+    void print_vector(std::vector<T>);
+
+    template <typename T>
+    void print_2d_vector(std::vector<std::vector<T>> vec);
+
+    template <typename T>
+    int calculate_2d_vector_size(std::vector<std::vector<T>> vec);
+
+}
+
+template <typename T>
+void vutil::print_vector(std::vector<T> vec) {
+    for (unsigned long int i = 0; i < 1; ++i) {
+        std::cout << "(" << i << ") |";
+        for (unsigned long int j = 0; j < vec.size(); ++j) {
+            if ((j + 1) == vec.size()) {
+                std::cout << std::setw(4) << vec.at(j) << "\n";
+            } else {
+                std::cout << std::setw(4) << vec.at(j);
+            }
+        }
+    }
+}
+
+template <typename T>
+void vutil::print_2d_vector(std::vector<std::vector<T>> vec) {
+    for (unsigned long int i = 0; i < vec.size(); i++) {
+        std::cout << "(" << i << ") |";
+        for (unsigned long int j = 0; j < vec.at(i).size(); ++j) {
+            std::cout << std::setw(4) << vec.at(i).at(j);
+        }
+        std::cout << "\n";
+    }
+}
+
+template <typename T>
+int vutil::calculate_2d_vector_size(std::vector<std::vector<T>> vec) {
+    int size = 0;
+    for (unsigned long int i = 0; i < vec.size(); i++)  {
+        size += vec.at(i).size();
+    }
+    return size;
+}
+
+
+#endif /* VUTIL_HPP */
